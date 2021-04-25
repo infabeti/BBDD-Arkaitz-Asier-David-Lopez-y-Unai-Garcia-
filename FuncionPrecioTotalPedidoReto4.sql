83327cb7ae543259024b33808fce604d6b8c7ac5
unai-garciagi <unai.garciagi@elorrieta-errekamari.com>
2021-04-25 23:35:21 +0200

    Finalizado el script del precio de las actividades

--- FuncionPrecioTotalPedidoReto4.sql
+++ FuncionPrecioTotalPedidoReto4.sql
@@ -4,27 +4,28 @@ returns float
 reads sql data
 
 begin
-declare PrecioTotalProducto float;
-declare CodigoDelPlato int;
-declare PVP float;
-declare Cantidad int;
-declare TipoObtenido enum('TICKET','FACTURA','COMANDA','PEDIDO','APROVISIONAMIENTO');
+	declare PrecioTotalProducto float;
+	declare PrecioTotalPlato float;
+	declare CodigoDelPlato int;
+	declare PVP float;
+	declare Cantidad int;
+	declare TipoObtenido enum('TICKET','FACTURA','COMANDA','PEDIDO','APROVISIONAMIENTO');
 
-select tipo into TipoObtenido from actividad where Transaccion = TransaccionRecibida;
+	select tipo into TipoObtenido from actividad where Transaccion = TransaccionRecibida;
 
-if TipoObtenido = 'TICKET' or 'FACTURA' or 'PEDIDO' then
-select sum(TotalProducto) into PrecioTotalProducto from lineaproducto where Transaccion = TransaccionRecibida;
-return PrecioTotalProducto;
+	if TipoObtenido = 'TICKET' or 'FACTURA' or 'PEDIDO' then
+		select sum(TotalProducto) into PrecioTotalProducto from lineaproducto where Transaccion = TransaccionRecibida;
+		return round(PrecioTotalProducto,2);
 
-elseif TipoObtenido = 'COMANDA' then
-select L.codigoplato into CodigoDelPlato from lineaplato L join plato P on L.codigoplato = P.codigoplato where Transaccion = TransaccionRecibida;
-select pvp,cantidad into PVP,Cantidad from lineaplato L join plato P on L.codigoplato = P.codigoplato where L.codigoplato = CodigoDelPlato;
-return sum(PVP*Cantidad);
-else 
-return 0;
+	elseif TipoObtenido = 'COMANDA' then
+		select sum(TotalProducto) into PrecioTotalProducto from lineaproducto where Transaccion = TransaccionRecibida;
+		select sum(P.pvp*L.cantidad) into PrecioTotalPlato from lineaplato L join plato P on L.codigoplato = P.codigoplato where Transaccion = TransaccionRecibida;
+		return round(PrecioTotalProducto + PrecioTotalPlato,2);
+	else 
+		return 0;
 
-end if;
+	end if;
 
-end//
+end;//
 
-select PrecioTotalPedido (15) "Precio Total";
+select PrecioTotalPedido (0) "Precio Total";
