8b4a2a1249d589c9eff5b7907c10c9dd0eec69ec
DavidLopez89 <david.lopezri@elorrieta-errekamari.com>
2021-05-07 12:23:06 +0200

    finalizado el algoritmo Naive-bayes

--- BBDD/algoritmo bayes.sql
+++ BBDD/algoritmo bayes.sql
@@ -45,20 +45,19 @@ select count(transaccion) into contadorProducto1
 from LineaProducto
 where codproducto1=codigoalimento and transaccion in
 				(select transaccion from actividad
-				where fecha between current_date() and (DATE_SUB(current_date(),INTERVAL 6 DAY)));
+				where fecha between (DATE_SUB(current_date(),INTERVAL 6 DAY)) and current_date());
 
 select count(transaccion) into contadorProducto2
 from LineaProducto
 where codproducto2=codigoalimento and transaccion in
 				(select transaccion from actividad
-				where fecha between current_date() and (DATE_SUB(current_date(),INTERVAL 6 DAY)));
+				where fecha between (DATE_SUB(current_date(),INTERVAL 6 DAY)) and current_date());
 
-select count(transaccion) into contadortransaccionestotales
+select count(Transaccion) into contadortransaccionestotales
 from actividad
-where fecha between current_date() and (DATE_SUB(current_date(),INTERVAL 6 DAY));
-select concat(contadortransaccionestotales) Resultado1;
+where fecha between (DATE_SUB(current_date(),INTERVAL 6 DAY)) and current_date();
 
-set probabilidadproductorelacion = contadorproducto2/contadorproducto1;
+set probabilidadproductorelacion = contadortransacciones/contadorproducto1;
 
 set probabilidadproducto1 = contadorproducto1/contadortransaccionestotales;
 
@@ -67,42 +66,11 @@ set probabilidadproducto2 = contadorproducto2/contadortransaccionestotales;
 set probabilidadproductototal = (probabilidadproductorelacion*probabilidadproducto1)/probabilidadproducto2;
 
 select concat('Coinciden ',contadortransacciones,' veces, el primer producto se ha comprado ',contadorProducto1,' veces y el segundo producto ',contadorProducto2,' veces.
-La probabilidad conjunta es ',probabilidadproductorelacion,', la probabilidad del primer producto ',probabilidadproducto1,', probabilidad del segundo producto ',probabilidadproducto2,' y la probabilidad de que coincidan es de ',probabilidadproductototal) Resultado;
+La probabilidad conjunta es ',probabilidadproductorelacion,', la probabilidad del primer producto ',probabilidadproducto1,', probabilidad del segundo producto ',probabilidadproducto2,'
+ y la probabilidad de que coincidan es de ',probabilidadproductototal) Resultado;
 
-/*
-select count(transacciones) into contadortransaccionestotales
-from actividades
-where fecha = current_date();
-
-
-select (contadorproducto1/contadortransaccionestotales) into probabilidadproducto1; 
-
-select (contadorproducto2/contadortransaccionestotales) into probabilidadproducto2; 
-
-select (probabilidadproducto1/probabilidadproducto2)/resultado into probabilidadproductototal; 
-
-
-
-if contadortransacciones > 1 then
-
-select resultado = contadortransaccionestotales((contadorProducto1*contadortransacciones)/contadorProducto2);
-
-		select concat("De las ",contadortransaccionestotales," compras realizadas en ",contadortransacciones,"se han comprado ",PrimerProducto," y ",SegundoProducto);
-        
-        select concat("De las ",contadorProducto1," que se han comprado ",PrimerProducto,"en un total de ",probabilidadproducto1);
-        
-		select concat("De las ",contadorProducto2," que se han comprado ",SegundoProducto,"en un total de ",probabilidadproducto2);
-        
-        select concat("Por lo tanto la probabilidad de comprar ",PrimerProducto," sera de ",probabilidadproductototal);
-        
-        
-else 
-
-		select	concat("No se han realizado coincidencias entre los dos productos");
-        
- end if;       
-*/
 end//
 
 call AlgoritmoNaiveBayes("Aquarius","Coca-cola");
+call AlgoritmoNaiveBayes("Aquarius","Aquarius");
 
